88d647eb84eeb672ba90119c9d49476eae15470c
GGrowDever <gabrielgrowdever@gmail.com>
2023-08-01 18:18:23 -0300

    incluido condicional que faltou no exercicio 5

--- atv5.js
+++ atv5.js
@@ -3,6 +3,7 @@ function verificaNumero(vetor, numero) {
 }
 
 let vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-let numero = 5;
+let numero = 11;
+let resultado = verificaNumero(vetor, numero);
 
 document.write(verificaNumero(vetor, numero));
